846146583ec6ed65e01d6f0e38d6fb650be35191
AlexanderEnriquezCCC <alex.enriquez@loop.colum.edu>
2022-03-23 18:58:25 -0500

    made changes to New_Manor, dialogue updated a little bit with Shamsa, placed new Camera UI stuff into Manor_New

--- Assets/DialogueSystem/GeneratedCode/Astronomer_Int_DialogueCode.cs
+++ Assets/DialogueSystem/GeneratedCode/Astronomer_Int_DialogueCode.cs
@@ -22,30 +22,26 @@ public bool shamsaTalkedTo = false;
 string name = "The Astronomer";
 Notes.Journal journal = Utility.Toolbox.Instance.Journal;
 int choices =0;
+int locationLearned = 0;
 
 
         public void Start()
         {
             // Setup //
-            dialogueChecks.Add("Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c0ae49f899c8414b8c81943b1c45082b",Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c0ae49f899c8414b8c81943b1c45082b);
-            dialogueChecks.Add("Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_571adf63a14c406898fdd5b55270f405",Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_571adf63a14c406898fdd5b55270f405);
-            dialogueChecks.Add("Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c7afa89ba4d04601b5270254eafba349",Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c7afa89ba4d04601b5270254eafba349);
-            eventFunctions.Add("Astronomer_Int_c1b5c56204294dc6a8dd6234469dee05",Astronomer_Int_c1b5c56204294dc6a8dd6234469dee05);
             conditionChecks.Add("Astronomer_Int_587b23ced7f14406beb04a840ad35495",Astronomer_Int_587b23ced7f14406beb04a840ad35495);
             eventFunctions.Add("Astronomer_Int_d2a00e74797e4ff6a354779009fbda74",Astronomer_Int_d2a00e74797e4ff6a354779009fbda74);
             eventFunctions.Add("Astronomer_Int_3be24e7b4af94f8195ed1d520b2e56ab",Astronomer_Int_3be24e7b4af94f8195ed1d520b2e56ab);
+            eventFunctions.Add("Astronomer_Int_c1b5c56204294dc6a8dd6234469dee05",Astronomer_Int_c1b5c56204294dc6a8dd6234469dee05);
+            dialogueChecks.Add("Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_571adf63a14c406898fdd5b55270f405",Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_571adf63a14c406898fdd5b55270f405);
+            dialogueChecks.Add("Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c7afa89ba4d04601b5270254eafba349",Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c7afa89ba4d04601b5270254eafba349);
+            dialogueChecks.Add("Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_bf886ecabd8744679e362bc1ee281072",Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_bf886ecabd8744679e362bc1ee281072);
+            conditionChecks.Add("Astronomer_Int_31076eaf81fd4a68843668e39cdb0698",Astronomer_Int_31076eaf81fd4a68843668e39cdb0698);
 
         }
 
         // Dialogue Checks //
         // From Node: 23e0ca1f-d5ed-485a-8a06-20257e18854e //
-        // Choice: Where were you at the time of the murder? - c0ae49f8-99c8-414b-8c81-943b1c45082b //
-        public bool Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_c0ae49f899c8414b8c81943b1c45082b()
-        {
-            return (true);
-        }
-        // From Node: 23e0ca1f-d5ed-485a-8a06-20257e18854e //
-        // Choice: Do you have any other information about the case?\n\n - 571adf63-a14c-4068-98fd-d5b55270f405 //
+        // Choice: Do you have any other information about the case? - 571adf63-a14c-4068-98fd-d5b55270f405 //
         public bool Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_571adf63a14c406898fdd5b55270f405()
         {
             return (true);
@@ -56,6 +52,12 @@ int choices =0;
         {
             return (true);
         }
+        // From Node: 23e0ca1f-d5ed-485a-8a06-20257e18854e //
+        // Choice: Where were you at the time of the murder? - bf886eca-bd87-4467-9e36-2bc1ee281072 //
+        public bool Astronomer_Int_23e0ca1fd5ed485a8a0620257e18854e_bf886ecabd8744679e362bc1ee281072()
+        {
+            return (true);
+        }
 
 
         // Condition Checks //
@@ -63,13 +65,13 @@ int choices =0;
         public bool Astronomer_Int_587b23ced7f14406beb04a840ad35495() {
             return (choices < 3);
         }
+        // Condition From Node: 31076eaf-81fd-4a68-8436-68e39cdb0698 //
+        public bool Astronomer_Int_31076eaf81fd4a68843668e39cdb0698() {
+            return (locationLearned > 0);
+        }
 
 
         // Event Functions //
-        // Event From Node: c1b5c562-0429-4dc6-a8dd-6234469dee05 //
-        public void Astronomer_Int_c1b5c56204294dc6a8dd6234469dee05() {
-journal.AddEntry("The Astronomer was in the infirmary.");
-        }
         // Event From Node: d2a00e74-797e-4ff6-a354-779009fbda74 //
         public void Astronomer_Int_d2a00e74797e4ff6a354779009fbda74() {
 journal.AddEntry("The Adventurer had a bloody nose.");
@@ -78,6 +80,11 @@ shamsaTalkedTo = true;
         // Event From Node: 3be24e7b-4af9-4f81-95ed-1d520b2e56ab //
         public void Astronomer_Int_3be24e7b4af94f8195ed1d520b2e56ab() {
 choices += 1;
+        }
+        // Event From Node: c1b5c562-0429-4dc6-a8dd-6234469dee05 //
+        public void Astronomer_Int_c1b5c56204294dc6a8dd6234469dee05() {
+journal.AddEntry("The Astronomer was in the infirmary.");
+locationLearned += 1;
         }
 
     }
