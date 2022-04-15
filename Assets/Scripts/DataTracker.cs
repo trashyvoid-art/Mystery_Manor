a6cc6ce687dfa6648163e9fa24705d3a44979802
Owen-Duffy <otd438@gmail.com>
2022-04-15 09:46:19 -0600

    Updated DataTracker and initialized it as singleton in Dialogue Graph

--- Assets/Scripts/DataTracker.cs
+++ Assets/Scripts/DataTracker.cs
@@ -7,123 +7,141 @@ using System;
 using System.Text.RegularExpressions;
 using System.Linq;
 
-public class DataTracker : MonoBehaviour
+public class DataTracker
 {
+    private static DataTracker _instance;
+    public static DataTracker Instance 
+    {
+        get
+        {
+            if (_instance == null) 
+            {
+                _instance = new DataTracker();
+            }
+            return _instance;
+        }
+    }
+
+    private DataTracker() 
+    {
+    
+    }
+
     //public int shamsaTalkedTo = 0;
-    string shamsaName = "The Astronomer";
-    Notes.Journal journal = Utility.Toolbox.Instance.Journal;
-    int choices = 0;
-    int locationLearned = 0;
+    //string shamsaName = "The Astronomer";
+    //Notes.Journal journal = Utility.Toolbox.Instance.Journal;
+    //int choices = 0;
+    //int locationLearned = 0;
 
-    public Astronomer_Int_DialogueCode shamsa;
-    public Sailor_Int_DialogueCode sean;
-    public Ms_Scheele_Int_DialogueCode scheele;
-    public Doctor_Int_DialogueCode sanchez;
-    public Adventurer_Int_DialogueCode sam;
-    public Psych_Body_DialogueCode scarlette;
+    //public Astronomer_Int_DialogueCode shamsa;
+    //public Sailor_Int_DialogueCode sean;
+    //public Ms_Scheele_Int_DialogueCode scheele;
+    //public Doctor_Int_DialogueCode sanchez;
+    //public Adventurer_Int_DialogueCode sam;
+    //public Psych_Body_DialogueCode scarlette;
 
-    public int shamsaTalkedTo = 0;
-    public int seanTalkedTo = 0;
-    public int scheeleTalkedTo = 0;
-    public int sanchezTalkedTo = 0;
-    public int samTalkedTo = 0;
-    public int scarletteTalkedTo = 0;
+    public int shamsaTalkedTo { get; set; } = 0;
+    public int seanTalkedTo { get; set; } = 0;
+    public int scheeleTalkedTo { get; set; } = 0;
+    public int sanchezTalkedTo { get; set; } = 0;
+    public int samTalkedTo { get; set; } = 0;
+    public int scarletteTalkedTo { get; set; } = 0;
 
     //ShamsaTalkedTo setter
-    public void setShamsaTalkedTo(int _shamsaTalkedTo)
-    {
-        shamsaTalkedTo = _shamsaTalkedTo;
-    }
+    //public void setShamsaTalkedTo(int _shamsaTalkedTo)
+    //{
+    //    shamsaTalkedTo = _shamsaTalkedTo;
+    //}
 
-    //ShamsaTalkedTo getter
-    public int getShamsaTalkedTo()
-    {
-        return shamsaTalkedTo;
-    }
-
-
-    //SeanTalkedTo setter
-    public void setSeanTalkedTo(int _seanTalkedTo)
-    {
-        seanTalkedTo = _seanTalkedTo;
-    }
-
-    //SeanTalkedTo getter
-    public int getSeanTalkedTo()
-    {
-        return seanTalkedTo;
-    }
+    ////ShamsaTalkedTo getter
+    //public int getShamsaTalkedTo()
+    //{
+    //    return shamsaTalkedTo;
+    //}
 
 
-    //ScheeleTalkedTo setter
-    public void setScheeleTalkedTo(int _scheeleTalkedTo)
-    {
-        scheeleTalkedTo = _scheeleTalkedTo;
-    }
+    ////SeanTalkedTo setter
+    //public void setSeanTalkedTo(int _seanTalkedTo)
+    //{
+    //    seanTalkedTo = _seanTalkedTo;
+    //}
 
-    //ScheeleTalkedTo getter
-    public int getScheeleTalkedTo()
-    {
-        return scheeleTalkedTo;
-    }
-
-
-    //SanchezTalkedTo setter
-    public void setSanchezTalkedTo(int _sanchezTalkedTo)
-    {
-        sanchezTalkedTo = _sanchezTalkedTo;
-    }
-
-    //SanchezTalkedTo getter
-    public int getSanchezTalkedTo()
-    {
-        return sanchezTalkedTo;
-    }
+    ////SeanTalkedTo getter
+    //public int getSeanTalkedTo()
+    //{
+    //    return seanTalkedTo;
+    //}
 
 
-    //SamTalkedTo setter
-    public void setSamTalkedTo(int _samTalkedTo)
-    {
-        samTalkedTo = _samTalkedTo;
-    }
+    ////ScheeleTalkedTo setter
+    //public void setScheeleTalkedTo(int _scheeleTalkedTo)
+    //{
+    //    scheeleTalkedTo = _scheeleTalkedTo;
+    //}
 
-    //SamTalkedTo getter
-    public int getSamTalkedTo()
-    {
-        return samTalkedTo;
-    }
+    ////ScheeleTalkedTo getter
+    //public int getScheeleTalkedTo()
+    //{
+    //    return scheeleTalkedTo;
+    //}
 
 
-    //ScarletteTalkedTo setter
-    public void setScarletteTalkedTo(int _scarletteTalkedTo)
-    {
-        scarletteTalkedTo = _scarletteTalkedTo;
-    }
+    ////SanchezTalkedTo setter
+    //public void setSanchezTalkedTo(int _sanchezTalkedTo)
+    //{
+    //    sanchezTalkedTo = _sanchezTalkedTo;
+    //}
 
-    //ScarletteTalkedTo getter
-    public int getScarletteTalkedTo()
-    {
-        return scarletteTalkedTo;
-    }
+    ////SanchezTalkedTo getter
+    //public int getSanchezTalkedTo()
+    //{
+    //    return sanchezTalkedTo;
+    //}
+
+
+    ////SamTalkedTo setter
+    //public void setSamTalkedTo(int _samTalkedTo)
+    //{
+    //    samTalkedTo = _samTalkedTo;
+    //}
+
+    ////SamTalkedTo getter
+    //public int getSamTalkedTo()
+    //{
+    //    return samTalkedTo;
+    //}
+
+
+    ////ScarletteTalkedTo setter
+    //public void setScarletteTalkedTo(int _scarletteTalkedTo)
+    //{
+    //    scarletteTalkedTo = _scarletteTalkedTo;
+    //}
+
+    ////ScarletteTalkedTo getter
+    //public int getScarletteTalkedTo()
+    //{
+    //    return scarletteTalkedTo;
+    //}
 
 
 
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        shamsa = new Astronomer_Int_DialogueCode();
-        sean = new Sailor_Int_DialogueCode();
-        scheele = new Ms_Scheele_Int_DialogueCode();
-        sanchez = new Doctor_Int_DialogueCode();
-        sam = new Adventurer_Int_DialogueCode();
-        scarlette = new Psych_Body_DialogueCode();
-}
+//    void Start()
+//    {
+//        //shamsa = new Astronomer_Int_DialogueCode();
+//        //sean = new Sailor_Int_DialogueCode();
+//        //scheele = new Ms_Scheele_Int_DialogueCode();
+//        //sanchez = new Doctor_Int_DialogueCode();
+//        //sam = new Adventurer_Int_DialogueCode();
+//        //scarlette = new Psych_Body_DialogueCode();
+//}
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-    }
+//    }
 }
