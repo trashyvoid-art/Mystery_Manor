b11ce2d89105d2907aa00bbe5f80261395264d2d
AlexanderEnriquezCCC <alex.enriquez@loop.colum.edu>
2022-04-29 00:58:04 -0500

    fixed pych_body dialogue that was missing

--- Assets/DialogueSystem/GeneratedCode/Psych_Body_DialogueCode.cs
+++ Assets/DialogueSystem/GeneratedCode/Psych_Body_DialogueCode.cs
@@ -30,19 +30,17 @@ int choices = 0;
         public void Start()
         {
             // Setup //
+            conditionChecks.Add("Psych_Body_939df39a2ae3488ca989ba6394d81f52",Psych_Body_939df39a2ae3488ca989ba6394d81f52);
+            eventFunctions.Add("Psych_Body_b2a41c09e3fe4890b19fd14b391ad51f",Psych_Body_b2a41c09e3fe4890b19fd14b391ad51f);
+            eventFunctions.Add("Psych_Body_0ad39360e60e4c0292ab6178b976c4b7",Psych_Body_0ad39360e60e4c0292ab6178b976c4b7);
+            conditionChecks.Add("Psych_Body_761caa1eb9724d338731c9953d7b45a9",Psych_Body_761caa1eb9724d338731c9953d7b45a9);
+            eventFunctions.Add("Psych_Body_791c15739eda4790af9bd0ef93a01c7b",Psych_Body_791c15739eda4790af9bd0ef93a01c7b);
+            conditionChecks.Add("Psych_Body_aab88120a9174c6fb20bfdcb6384cfb7",Psych_Body_aab88120a9174c6fb20bfdcb6384cfb7);
+            conditionChecks.Add("Psych_Body_f1c70959a853458aaa312421f36b7a69",Psych_Body_f1c70959a853458aaa312421f36b7a69);
+            eventFunctions.Add("Psych_Body_37d5097f6e1947bab51fd27057cd9537",Psych_Body_37d5097f6e1947bab51fd27057cd9537);
             dialogueChecks.Add("Psych_Body_f122eae0c0054e53924aeda199421940_7db36f922d7d4486a45c0c56f1fa7dea",Psych_Body_f122eae0c0054e53924aeda199421940_7db36f922d7d4486a45c0c56f1fa7dea);
             dialogueChecks.Add("Psych_Body_f122eae0c0054e53924aeda199421940_20212544bc2243e6a4c183dc617dd63a",Psych_Body_f122eae0c0054e53924aeda199421940_20212544bc2243e6a4c183dc617dd63a);
             dialogueChecks.Add("Psych_Body_f122eae0c0054e53924aeda199421940_2d199e62b0bc4e5e93c55e7cf2026b89",Psych_Body_f122eae0c0054e53924aeda199421940_2d199e62b0bc4e5e93c55e7cf2026b89);
-            conditionChecks.Add("Psych_Body_939df39a2ae3488ca989ba6394d81f52",Psych_Body_939df39a2ae3488ca989ba6394d81f52);
-            eventFunctions.Add("Psych_Body_b2a41c09e3fe4890b19fd14b391ad51f",Psych_Body_b2a41c09e3fe4890b19fd14b391ad51f);
-            conditionChecks.Add("Psych_Body_761caa1eb9724d338731c9953d7b45a9",Psych_Body_761caa1eb9724d338731c9953d7b45a9);
-            eventFunctions.Add("Psych_Body_791c15739eda4790af9bd0ef93a01c7b",Psych_Body_791c15739eda4790af9bd0ef93a01c7b);
-            eventFunctions.Add("Psych_Body_37d5097f6e1947bab51fd27057cd9537",Psych_Body_37d5097f6e1947bab51fd27057cd9537);
-            conditionChecks.Add("Psych_Body_f1c70959a853458aaa312421f36b7a69",Psych_Body_f1c70959a853458aaa312421f36b7a69);
-            eventFunctions.Add("Psych_Body_a1c2a242078542ee800038ab807a165f",Psych_Body_a1c2a242078542ee800038ab807a165f);
-            eventFunctions.Add("Psych_Body_50ae30f288044c9d949e3b6dc5c623a6",Psych_Body_50ae30f288044c9d949e3b6dc5c623a6);
-            conditionChecks.Add("Psych_Body_aab88120a9174c6fb20bfdcb6384cfb7",Psych_Body_aab88120a9174c6fb20bfdcb6384cfb7);
-            eventFunctions.Add("Psych_Body_0ad39360e60e4c0292ab6178b976c4b7",Psych_Body_0ad39360e60e4c0292ab6178b976c4b7);
 
         }
 
@@ -76,13 +74,13 @@ int choices = 0;
         public bool Psych_Body_761caa1eb9724d338731c9953d7b45a9() {
             return (body > 0);
         }
-        // Condition From Node: f1c70959-a853-458a-aa31-2421f36b7a69 //
-        public bool Psych_Body_f1c70959a853458aaa312421f36b7a69() {
-            return (blood > 0);
-        }
         // Condition From Node: aab88120-a917-4c6f-b20b-fdcb6384cfb7 //
         public bool Psych_Body_aab88120a9174c6fb20bfdcb6384cfb7() {
             return (hands > 0);
+        }
+        // Condition From Node: f1c70959-a853-458a-aa31-2421f36b7a69 //
+        public bool Psych_Body_f1c70959a853458aaa312421f36b7a69() {
+            return (blood > 0);
         }
 
 
@@ -91,29 +89,23 @@ int choices = 0;
         public void Psych_Body_b2a41c09e3fe4890b19fd14b391ad51f() {
 choices += 1;
         }
-        // Event From Node: 791c1573-9eda-4790-af9b-d0ef93a01c7b //
-        public void Psych_Body_791c15739eda4790af9bd0ef93a01c7b() {
-journal.AddEntry("Psychologist's body had two stab wounds in the abdomen area.");
-dt.clues += 1;
-        }
-        // Event From Node: 37d5097f-6e19-47ba-b51f-d27057cd9537 //
-        public void Psych_Body_37d5097f6e1947bab51fd27057cd9537() {
-journal.AddEntry("Footprints and trail of blood from Scarlette's body leads out further into the office, but end abrubtly.");
-dt.clues += 1;
-        }
-        // Event From Node: a1c2a242-0785-42ee-8000-38ab807a165f //
-        public void Psych_Body_a1c2a242078542ee800038ab807a165f() {
-body += 1;
-        }
-        // Event From Node: 50ae30f2-8804-4c9d-949e-3b6dc5c623a6 //
-        public void Psych_Body_50ae30f288044c9d949e3b6dc5c623a6() {
-blood += 1;
-        }
         // Event From Node: 0ad39360-e60e-4c02-92ab-6178b976c4b7 //
         public void Psych_Body_0ad39360e60e4c0292ab6178b976c4b7() {
 journal.AddEntry("The assailant had dark hair? Well that could be almost anyone...");
 dt.clues += 1;
 hands += 1;
+        }
+        // Event From Node: 791c1573-9eda-4790-af9b-d0ef93a01c7b //
+        public void Psych_Body_791c15739eda4790af9bd0ef93a01c7b() {
+journal.AddEntry("Psychologist's body had two stab wounds in the abdomen area.");
+dt.clues += 1;
+body += 1;
+        }
+        // Event From Node: 37d5097f-6e19-47ba-b51f-d27057cd9537 //
+        public void Psych_Body_37d5097f6e1947bab51fd27057cd9537() {
+journal.AddEntry("Footprints and trail of blood from Scarlette's body leads out further into the office, but end abrubtly.");
+dt.clues += 1;
+blood += 1;
         }
 
     }
