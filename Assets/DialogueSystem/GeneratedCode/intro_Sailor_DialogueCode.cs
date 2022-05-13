1ed213030684b31d68c6759743bc8374ad5ecc31
Owen-Duffy <otd438@gmail.com>
2022-05-13 11:46:39 -0600

    Revert "Updated intro dialogue"
    This reverts commit 9943ebc79ef4718e8e45ca770b14d3fcffdf946e.

--- Assets/DialogueSystem/GeneratedCode/intro_Sailor_DialogueCode.cs
+++ Assets/DialogueSystem/GeneratedCode/intro_Sailor_DialogueCode.cs
@@ -24,11 +24,6 @@ string name = "The Sailor";
         public void Start()
         {
             // Setup //
-            eventFunctions.Add("intro_Sailor_e263ef08067945db9df862fd7bd66844",intro_Sailor_e263ef08067945db9df862fd7bd66844);
-            eventFunctions.Add("intro_Sailor_f1a6b74c813845b6976fb45147ea6501",intro_Sailor_f1a6b74c813845b6976fb45147ea6501);
-            eventFunctions.Add("intro_Sailor_c53d7ebb61e54be0b7b597e8908598cb",intro_Sailor_c53d7ebb61e54be0b7b597e8908598cb);
-            eventFunctions.Add("intro_Sailor_e3f4fd1c8ffc4a018ec165dfc9624377",intro_Sailor_e3f4fd1c8ffc4a018ec165dfc9624377);
-            eventFunctions.Add("intro_Sailor_77535bc76510422e90ad0e138eff9d2e",intro_Sailor_77535bc76510422e90ad0e138eff9d2e);
 
         }
 
@@ -39,30 +34,6 @@ string name = "The Sailor";
 
 
         // Event Functions //
-        // Event From Node: e263ef08-0679-45db-9df8-62fd7bd66844 //
-        public void intro_Sailor_e263ef08067945db9df862fd7bd66844() {
-GameObject.FindObjectOfType<AudioManager>().Play("Next Line");
-GameObject.FindObjectOfType<AudioManager>().Play("Sean 3");
-        }
-        // Event From Node: f1a6b74c-8138-45b6-976f-b45147ea6501 //
-        public void intro_Sailor_f1a6b74c813845b6976fb45147ea6501() {
-GameObject.FindObjectOfType<AudioManager>().Play("Next Line");
-GameObject.FindObjectOfType<AudioManager>().Play("Sean 4");
-        }
-        // Event From Node: c53d7ebb-61e5-4be0-b7b5-97e8908598cb //
-        public void intro_Sailor_c53d7ebb61e54be0b7b597e8908598cb() {
-GameObject.FindObjectOfType<AudioManager>().Play("End Convo");
-        }
-        // Event From Node: e3f4fd1c-8ffc-4a01-8ec1-65dfc9624377 //
-        public void intro_Sailor_e3f4fd1c8ffc4a018ec165dfc9624377() {
-GameObject.FindObjectOfType<AudioManager>().Play("Start Convo");
-GameObject.FindObjectOfType<AudioManager>().Play("Sean 1");
-        }
-        // Event From Node: 77535bc7-6510-422e-90ad-0e138eff9d2e //
-        public void intro_Sailor_77535bc76510422e90ad0e138eff9d2e() {
-GameObject.FindObjectOfType<AudioManager>().Play("Next Line");
-GameObject.FindObjectOfType<AudioManager>().Play("Sean 2");
-        }
 
     }
 }
