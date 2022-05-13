469a703aa6ee077b4d9c8d6ead71f6bc244fd4a5
Owen-Duffy <otd438@gmail.com>
2022-05-13 12:01:53 -0600

    Fixed fuckery??

---
+++ Assets/DialogueSystem/GeneratedCode/Nasr_DialogueCode.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEditor;
+using DialogueSystem;
+using System.Reflection;
+
+namespace DialogueSystem.Code
+{
+    public class Nasr_DialogueCode : DialogueCode, IDialogueCode
+    {
+        public Nasr_DialogueCode() 
+        {
+            Start();
+        }
+
+
+        // Variables //
+
+
+        public void Start()
+        {
+            // Setup //
+
+        }
+
+        // Dialogue Checks //
+
+
+        // Condition Checks //
+
+
+        // Event Functions //
+
+    }
+}
--- Assets/DialogueSystem/GeneratedCode/Triceratops_DialogueCode.cs
+++ Assets/DialogueSystem/GeneratedCode/Triceratops_DialogueCode.cs
@@ -17,14 +17,11 @@ namespace DialogueSystem.Code
 
 
         // Variables //
-        // Variable(s) From Node: 4e002274-93b6-471e-902a-79dcabe25f03 //
-Notes.Journal journal = Utility.Toolbox.Instance.Journal;
 
 
         public void Start()
         {
             // Setup //
-            eventFunctions.Add("Triceratops_4dec9dff622f4d6996420beed3809e7d",Triceratops_4dec9dff622f4d6996420beed3809e7d);
 
         }
 
@@ -35,10 +32,6 @@ Notes.Journal journal = Utility.Toolbox.Instance.Journal;
 
 
         // Event Functions //
-        // Event From Node: 4dec9dff-622f-4d69-9642-0beed3809e7d //
-        public void Triceratops_4dec9dff622f4d6996420beed3809e7d() {
-journal.AddEntry("A medium sized plant with large oval leaves that are light in the middle and dark around the edges.");
-        }
 
     }
 }
