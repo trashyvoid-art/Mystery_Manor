1d41c94d5609976f657ad3fce39cac0d84a4a122
Owen-Duffy <otd438@gmail.com>
2022-05-13 11:46:37 -0600

    Revert "Fixed merge"
    This reverts commit ce7796d4ceef8bb05f31c02556be006f36389446, reversing
    changes made to 9943ebc79ef4718e8e45ca770b14d3fcffdf946e.

--- Assets/DialogueSystem/GeneratedCode/Triceratops_DialogueCode.cs
+++ Assets/DialogueSystem/GeneratedCode/Triceratops_DialogueCode.cs
@@ -17,11 +17,14 @@ namespace DialogueSystem.Code
 
 
         // Variables //
+        // Variable(s) From Node: 4e002274-93b6-471e-902a-79dcabe25f03 //
+Notes.Journal journal = Utility.Toolbox.Instance.Journal;
 
 
         public void Start()
         {
             // Setup //
+            eventFunctions.Add("Triceratops_4dec9dff622f4d6996420beed3809e7d",Triceratops_4dec9dff622f4d6996420beed3809e7d);
 
         }
 
@@ -32,6 +35,10 @@ namespace DialogueSystem.Code
 
 
         // Event Functions //
+        // Event From Node: 4dec9dff-622f-4d69-9642-0beed3809e7d //
+        public void Triceratops_4dec9dff622f4d6996420beed3809e7d() {
+journal.AddEntry("A medium sized plant with large oval leaves that are light in the middle and dark around the edges.");
+        }
 
     }
 }
--- Assets/Scripts/LoadNextScene_DeleteLater.cs
+++ Assets/Scripts/LoadNextScene_DeleteLater.cs
@@ -20,11 +20,6 @@ public class LoadNextScene_DeleteLater : MonoBehaviour
         }
     }
 
-    public void OnDinnerComplete()
-    {
-        SceneManager.LoadScene(NextLevelLoad);
-    }
-
     //public void GoToIntro()
     //{
     //    SceneManager.LoadScene(1);
